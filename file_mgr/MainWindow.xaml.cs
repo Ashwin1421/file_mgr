1a7462591423e5fed7d1213105a8e117a87f0fb6
Ashwin <ashwin.joshi12@live.com>
2017-02-15 23:03:05 -0600

    First commit

---
+++ file_mgr/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+
+namespace file_mgr
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string path1, path2;
+
+        private void SourcePathSelector_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                path1 = dialog.SelectedPath;
+                SourcePath.Text = path1;
+            }
+        }
+
+        private void DestinationPathSelector_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                path2 = dialog.SelectedPath;
+                DestinationPath.Text = path2;
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+  
+    }
+}
